987d6b34902a2395bac9dbfaca589813e2a0043d
hacles100 <haclesrocha200@gmail.com>
2023-04-12 15:50:07 -0100

    increment function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  // const incrementCount = () => {
+  //   setCount(count + 1);
+  // }
+
+  const incrementCount = () => {
+    setCount((prevState) => prevState + 1);
+  };
+
   return (
-    <div>
-      <h1>0</h1>
-      <button>Increment</button>
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={incrementCount}>Increment</button>
     </div>
   );
 }
